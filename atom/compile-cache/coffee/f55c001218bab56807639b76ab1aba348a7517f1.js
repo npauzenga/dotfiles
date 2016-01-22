5130e49b2eb66626a6c321c41a363ce297ab7371
Nate <npauzenga@gmail.com>
2016-01-22 13:18:29 -0500

    delete atom compile-cache

--- atom/compile-cache/coffee/f55c001218bab56807639b76ab1aba348a7517f1.js
+++
@@ -1,8 +0,0 @@
-(function() {
-
-
-}).call(this);
-
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAiZmlsZSI6ICIiLAogICJzb3VyY2VSb290IjogIiIsCiAgInNvdXJjZXMiOiBbCiAgICAiL1VzZXJzL25hdGUvLmF0b20vaW5pdC5jb2ZmZWUiCiAgXSwKICAibmFtZXMiOiBbXSwKICAibWFwcGluZ3MiOiAiQUFBQTtBQUFBO0FBQUEiCn0=
-
-//# sourceURL=/Users/nate/.atom/init.coffee
